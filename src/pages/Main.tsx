85bc2c719f1a59af7af3c9cac08736ae7ccaf253
nikita azizov <nik.azizoff2016@gmail.com>
2024-04-15 12:16:58 +0300

    Main: add title and form panel with nickname input field and link to Quiz component Quiz: add title and form panel with "prev" and "next" buttons add onPrev and onNext functions that display text in console

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Main() {
-    return <h1>Main!</h1>;
+    return (
+        <div className="container mx-auto h-full flex flex-col items-center justify-around">
+            <h1 className="text-6xl mt-2 p-5 border-4 border-blue-500">Quiz app</h1>
+            <div className="form-panel w-1/3 flex flex-col gap-6 items-center">
+                <input
+                    type="text"
+                    className="border border-zinc-900 rounded-lg block p-2.5 w-full mt-6"
+                    placeholder="Enter your nickname"
+                />
+                <div className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-m px-5 py-2.5 text-center mb-2 w-full">
+                    <Link to={'quiz'}>Start quiz</Link>
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,5 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Quiz() {
-    return <div> Quiz</div>;
+    const onNext = () => {
+        console.log('On next click');
+    };
+
+    const onPrev = () => {
+        console.log('On prev click');
+    };
+
+    return (
+        <div className="container mx-auto h-full flex flex-col items-center justify-around">
+            <h1 className="text-6xl mt-2 p-5 border-4 border-blue-500">Quiz app</h1>
+
+            {/* Display questions */}
+
+            <div className="form-panel w-1/3 flex justify-between">
+                <button
+                    className="text-white bg-blue-500 hover:bg-blue-600 focus:ring-2 focus:ring-blue-300 font-medium rounded-lg text-lg px-10 py-2.5 focus:outline-none"
+                    onClick={onPrev}>
+                    Prev
+                </button>
+                <button
+                    className="text-white bg-blue-500 hover:bg-blue-600 focus:ring-2 focus:ring-blue-300 font-medium rounded-lg text-lg px-10 py-2.5 focus:outline-none"
+                    onClick={onNext}>
+                    Next
+                </button>
+            </div>
+        </div>
+    );
 }
